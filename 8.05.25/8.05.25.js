280a25c7eec4cc2bc805ee367eb1d566e3d56a4e
Арсений <Penkovskiy2024@mail.ru>
2025-05-11 01:43:14 +0300

    Add files via upload

---
+++ 8.05.25/8.05.25.js
@@ -0,0 +1,16 @@
+// Задача 1
+// function sequentialTasks(phrase, who) {
+//   alert( phrase + ', ' + who );
+// }
+
+// setTimeout(sequentialTasks, 1000, "Первая задача завершена");
+// setTimeout(sequentialTasks, 2000, "Вторая задача завершена");
+// setTimeout(sequentialTasks, 2000, "Все задачи выполнены");
+
+//  Задача 2
+// function delayMessage(message, delay) {
+//     setTimeout(() => {
+//         console.log(message);
+//     }, delay);
+// }
+// delayMessage("Привет, мир!", 2000); 
