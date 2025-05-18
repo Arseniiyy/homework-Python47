84b26c8e1aef9c3e978d8e6c87c07b6d631f61d4
Арсений <Penkovskiy2024@mail.ru>
2025-05-18 23:41:27 +0300

    Add files via upload

---
+++ 23.05.25/23.05.js
@@ -0,0 +1,39 @@
+// // Задание 1
+// let headerNew = document.getElementById('header')
+// console.log('Новый элемент',headerNew)
+
+// let itemsNew = document.getElementsByClassName('item')
+// console.log('Новый элемент',itemsNew)
+
+// const divsNew = document.getElementsByTagName('div')
+// console.log('Новый элемент',divsNew)
+
+//Задание 2
+// let headerNew = document.getElementById('header');
+// console.log('Новый элемент', headerNew);
+
+// headerNew.textContent = 'Новый заголовок';
+
+// headerNew.innerHTML += ' <span>Это внутри спана</span>';
+
+// headerNew.style.color = 'blue';
+
+// headerNew.style.fontSize = '24px';
+
+// Задание 3
+// Находим первый элемент с классом "item"
+// let itemElement = document.querySelector('.item');
+
+// itemElement.setAttribute('data-id', '123');
+
+// let dataIdValue = itemElement.getAttribute('data-id');
+// console.log('Значение data-id:', dataIdValue);
+
+// itemElement.setAttribute('data-id', '456');
+
+// let updatedDataIdValue = itemElement.getAttribute('data-id');
+// console.log('Обновленное значение data-id:', updatedDataIdValue);
+
+// Задание 4
+// const newDiv = document.createElement('div');
+// newDiv.className = 'new-item';
---
+++ 23.05.25/23.05.py
@@ -0,0 +1,52 @@
+# 1.
+# number =1 
+# print(number)
+
+#2
+# string = "Моя первая переменная"
+# print(len(string))
+
+#3
+# my_bool = True
+# print(my_bool)
+
+# 4
+# num = int(input("Введите целое число: "))
+# square = num ** 2
+# print("Квадрат числа:", square)
+
+# 5
+# for i in range(1, 11):
+#     print(i)
+
+# 6
+# num = 10
+# while num >= 1:
+#     print(num)
+#     num -= 1
+
+# 7
+# num = float(input("Введите число: "))
+
+# if num > 0:
+#     print("Число положительное")
+# elif num < 0:
+#     print("Число отрицательное")
+# else:
+#     print("Число равно нулю")
+
+# 8
+# def сумма(a, b):
+#     return a + b
+
+# 9
+# def в_верхнем_регистре(строка):
+#     return строка.upper()
+
+# 10
+# числа = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for число in числа:
+#     if число % 2 == 0:
+#         print(f"{число} — чётное")
+#     else:
+#         print(f"{число} — нечётное")
